205e41538468d44fd174567ce2471e94e3f0f7b1
Gabriel Eudes <gaeudes@student.42.fr>
2025-10-18 17:44:01 +0200

    [NOTHING]

--- inc/WebServ.hpp
+++ inc/WebServ.hpp
@@ -6,7 +6,7 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 10:44:46 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/10/15 18:49:03 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/10/18 17:09:15 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,8 @@ class WebServ
 	public:
 		WebServ(const int ac, char *av[], char *env[]);
 		~WebServ(void);
+
 };
+
 
 #endif

3e2af0fd2fc4bb0da7bc214a48edb2e0c659d880
Gabriel Eudes <gaeudes@student.42.fr>
2025-11-17 11:26:49 +0100

    Simple cgi scripts added

---
+++ .pageHTLM/cgi-bin/CGI_test.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php-cgi
+Content-type: text/html
+
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hello from PHP CGI!</h1>
+</body>
+</html>
---
+++ .pageHTLM/cgi-bin/CGI_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print("Content-Type: text/html\n")
+print("""
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hello from Python CGI!</h1>
+</body>
+</html>
+""")
